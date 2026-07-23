52b17b17a13e00ce1954c183b2f794a73708b164
Gautam Bharath <gautam.b1306@gmail.com>
2026-07-23 10:32:16 +0530

    Initialize App component with ChatRoom

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatRoom from "./components/ChatRoom";
+
+function App() {
+  return (
+    <div>
+      <h1>One Voice</h1>
+      <ChatRoom />
+    </div>
+  );
+}
+
+export default App;
