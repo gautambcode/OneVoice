4441580dc2721cf5028fddf978d525afef345d57
Gautam Bharath <gautam.b1306@gmail.com>
2026-07-23 10:30:12 +0530

    Initialize Express server with Socket.io support

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("New user connected");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("receiveMessage", { text: message, time: new Date() });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(5000, () => console.log("Server running on port 5000"));
